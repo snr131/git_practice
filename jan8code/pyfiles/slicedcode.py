6469e063c1c2a8c9ee011aa525a89f2af81bef8b
snr131 <53094202+snr131@users.noreply.github.com>
2021-01-08 11:33:07 -0500

    a

---
+++ jan8code/pyfiles/slicedcode.py
@@ -0,0 +1,11 @@
+family_members = ['Sarah', 'Scott', 'Kaia', 'Finley', 'Gulzar', 'Diane', 'Jill', 'David', 'Noah', 'Ari', 'Alyssa', 'Louis', 'Zoe', 'Habiba', 'Jalal', 'Aziz', 'Noorallah', 'Nazlin', 'Shazia', 'Tahseen']
+print(family_members[0:2])
+print(family_members[:2])
+print(family_members[0:-2])
+print(family_members[5:])
+print(family_members[5:7])
+print(family_members[0:2])
+print(family_members[0:10:2])
+print(family_members[0:10:4])
+print(family_members[:10:4])
+print(family_members[1:9:2])
