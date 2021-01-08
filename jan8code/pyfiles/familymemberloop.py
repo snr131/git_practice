831581f87b9a7f832ed91486c18560848c8e427f
snr131 <53094202+snr131@users.noreply.github.com>
2021-01-08 11:40:09 -0500

    b

---
+++ jan8code/pyfiles/familymemberloop.py
@@ -0,0 +1,3 @@
+family_members = ['Sarah', 'Scott', 'Kaia', 'Finley', 'Gulzar', 'Diane', 'Jill', 'David', 'Noah', 'Ari', 'Alyssa', 'Louis', 'Zoe', 'Habiba', 'Jalal', 'Aziz', 'Noorallah', 'Nazlin', 'Shazia', 'Tahseen']
+for member in family_members:
+  print("Happy New Year, %s!" % (member))
