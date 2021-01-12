e1197127186f9e4e97c3bf13a161983fc1b3d439
snr131 <53094202+snr131@users.noreply.github.com>
2021-01-12 09:38:41 -0500

    b

--- Jan122021Puzzle.py
+++ Jan122021Puzzle.py
@@ -1,11 +1,20 @@
+# this function (called function) will sum all integers from 0 to num
 def function(num):
+  # the variable result will hold a list, from 0 to num
   result = [*range(0,num)]
+  # the variable sum will hold the sum of the result list
   sum = 0
   #print(result)
+  # the for loop below will add each element of the result list to the sum variable
   for i in range(0, len(result)):
     sum = sum + result[i]
+    # if the function input "num" is not an integer 0 will be returned along with a message
     if type(num) != int:
       return 0
       print('wrong input... BYE!')
+  # if the function input "num" is an integer, the sum will be printed
   print(sum)
+
+
+function(4)
   
